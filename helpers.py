aa981698808b63771599c247a1121ac6cc32cbce
jbr46 <108296874+jbr46@users.noreply.github.com>
2022-09-17 23:31:24 +0100

    integrate with cloud sql

--- helpers.py
+++ helpers.py
@@ -2,13 +2,18 @@ import os
 import requests
 import urllib.parse
 import random
+import pymysql.cursors
 
 from flask import redirect, render_template, request, session
 from functools import wraps
-from cs50 import SQL
 from datetime import datetime
 
-db = SQL("sqlite:///constituencies.db")
+connection = pymysql.connect(host='34.68.42.218',
+                             user='jbr46',
+                             password='constituencies',
+                             database='constituencies',
+                             charset='utf8',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 
 def apology(message, code=400):
@@ -40,23 +45,49 @@ def login_required(f):
     return decorated_function
 
 def generate_constituency():
-    constituency = db.execute("SELECT MP, party, constituency FROM constituencies WHERE id = ?", random.randint(0, 648))[0]
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT `MP`, `party`, `constituency` FROM `constituencies` WHERE `id` = %i"
+            cursor.execute(sql, (random.randint(0, 648),))
+            constituency = cursor.fetchone()[0]
+
+    #constituency = db.execute("SELECT MP, party, constituency FROM constituencies WHERE id = ?", random.randint(0, 648))[0]
     return constituency
 
 def get_personal_bests(id):
-    bests = db.execute("SELECT score, date FROM bests WHERE id = ? ORDER BY score DESC LIMIT 5", id)
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT `score`, `date` FROM `bests` WHERE `id` = %i ORDER BY `score` DESC LIMIT 5"
+            cursor.execute(sql, (id,))
+            bests = cursor.fetchone()
+
+    #bests = db.execute("SELECT score, date FROM bests WHERE id = ? ORDER BY score DESC LIMIT 5", id)
     return bests
 
 def add_bests(score, username, id, bests):
-    now = datetime.now()
-    try:
-        if score > bests[4]["score"]:
-            db.execute("INSERT INTO bests (score, date, username, id) VALUES (?, ?, ?, ?)", score, now.strftime("%d/%m/%Y"), username, id)
-    except IndexError:
-            db.execute("INSERT INTO bests (score, date, username, id) VALUES (?, ?, ?, ?)", score, now.strftime("%d/%m/%Y"), username, id)
+    with connection:
+        now = datetime.now()
+        try:
+            if score > bests[4]["score"]:
+                with connection.cursor() as cursor:
+                    sql = "INSERT INTO `bests` (`score`, `date`, `username`, `id`) VALUES (%i, %s, %s, %i)"
+                    cursor.execute(sql, (score, now.strftime("%d/%m/%Y"), username, id,))
+                connection.commit()
+
+                #db.execute("INSERT INTO bests (score, date, username, id) VALUES (?, ?, ?, ?)", score, now.strftime("%d/%m/%Y"), username, id)
+        except IndexError:
+                with connection.cursor() as cursor:
+                    sql = "INSERT INTO `bests` (`score`, `date`, `username`, `id`) VALUES (%i, %s, %s, %i)"
+                    cursor.execute(sql, (score, now.strftime("%d/%m/%Y"), username, id,))
+                connection.commit()
     return
 
 def get_bests():
-    bests = db.execute("SELECT score, username, date FROM bests ORDER BY score DESC LIMIT 5")
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT `score`, `username`, `date` FROM `bests` ORDER BY `score` DESC LIMIT 5"
+            cursor.execute(sql)
+            bests = cursor.fetchone()
     return bests
+
 
